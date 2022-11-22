72673b4458871c2b87424452c3fda804b6eedd1a
명현 <z9x80123@gmail.com>
2022-11-22 22:55:36 +0900

    feat/bookmark bookmark 기능 만들기, bookmark url수정

---
+++ mypage/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from rest_framework import status
+from product.models import Products
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = "__all__"
+
--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    
+
     path('signup/', views.UserView.as_view(), name='user_signip'),
     path('login/', views.UserLogin.as_view(), name='user_login'),
     path('delete/', views.UserDelete.as_view(), name='user_delete'),
-    
+
 ]
