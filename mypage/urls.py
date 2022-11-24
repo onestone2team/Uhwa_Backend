e2d12400d9a958f78829339647c39b7670e635b1
hyojine <touroz@naver.com>
2022-11-24 12:11:23 +0900

    refact: Mypage 앱 내 시리얼라이저 및 메소드 이름 수정

--- mypage/urls.py
+++ mypage/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 urlpatterns = [
 
     path('', views.ProfileView.as_view(), name='profile_view'),
-    path('bookmarklist/', views.ProfileBookmark.as_view(), name='profile_bookmark'),
-    path('myproducts/', views.ProfileMyProducts.as_view(), name='profile_product'),
-    path('myorderlist/', views.ProfileMyOrderlis.as_view(), name='profile_order'),
+    path('bookmarklist/', views.MyBookmarkView.as_view(), name='profile_bookmark'),
+    path('myproducts/', views.MyProductsView.as_view(), name='profile_product'),
+    path('myorderlist/', views.MyOrderlistView.as_view(), name='profile_order'),
 
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -1,14 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
 from product.models import Products
-from mypage.serializer import BookmarkSerializer, MyProductListSerializer, ProfileMyOrderlistSerializer
+from mypage.serializer import MyBookmarkListSerializer, MyProductListSerializer, MyOrderListSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from product.models import Products
 from order.models import Orders
 from rest_framework.generics import get_object_or_404
-# Create your views here.
-class ProfileView(APIView):             #회원 정보 확인 및 수정
+
+
+#회원 정보 확인 및 수정
+class ProfileView(APIView):             
 
     def get(self, request):
         pass
@@ -16,28 +18,30 @@ class ProfileView(APIView):             #회원 정보 확인 및 수정
     def put(self, request):
         pass
 
-class ProfileBookmark(APIView):         #bookmarklist/ 찜한 상품 리스트
+#bookmarklist/ 찜한 상품 리스트
+class MyBookmarkView(APIView):         
     permission_classes=[permissions.IsAuthenticated]
 
     def get(self, request):
         bookmark_list = Products.objects.filter(bookmark=request.user.id)
         if bookmark_list:
-            serializer = BookmarkSerializer(bookmark_list, many=True)
+            serializer = MyBookmarkListSerializer(bookmark_list, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"message":"잘못된 접근입니다!"}, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ProfileMyProducts(APIView):       #myproducts/ 내가 만든 상품 리스트
+#myproducts/ 내가 만든 상품 리스트
+class MyProductsView(APIView):      
 
     def get(self, request):
         product = Products.objects.filter(user_id=request.user.id)
         serializer = MyProductListSerializer(product, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class ProfileMyOrderlis(APIView):      #myorderlist/ 나의 주문 목록
+#myorderlist/ 나의 주문 목록
+class MyOrderlistView(APIView):      
 
     def get(self, request):
         product = Orders.objects.filter(user_id=request.user.id)
-        serializer = ProfileMyOrderlistSerializer(product, many=True)
+        serializer = MyOrderListSerializer(product, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
